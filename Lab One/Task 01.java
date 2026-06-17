7622b19d69b7d3e95aca6c05d0fe6d9d21448e32
Kazi Shaki Jawhar <shakijawhar@gmail.com>
2026-06-18 01:11:02 +0600

    Add Shaki class with student information display

---
+++ Lab One/Task 01.java
@@ -0,0 +1,16 @@
+public class Shaki {
+    public static void main(String[] args) {
+
+        String name = "Kazi Shaki Jawhar";
+        String studentId = "E253026";
+        int age = 21;
+        String phone = "01761078842";
+        String email = "shakijawhar@gmail.com";
+        String address = "Senbag, Noakhali";
+
+        System.out.printf(
+                "Name: %s\nStudentId: %s\nAge: %d\nPhone: %s\nEmail: %s\nAddress: %s\n",
+                name, studentId, age, phone, email, address
+        );
+    }
+}
