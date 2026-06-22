2273a084ad704fe426355fccc3a5798af2164361
Kazi Shaki Jawhar <shakijawhar@gmail.com>
2026-06-22 12:05:45 +0600

    Update Task_2.java

--- Lab Seven/Task_2.java
+++ Lab Seven/Task_2.java
@@ -1,28 +1,22 @@
-lass Shape { 
-  void draw() { 
-    System.out.println("Drawing Shape"); 
-  }
-} 
-class Circle extends Shape { 
-  @Override 
-  void draw() { 
-    System.out.println("Drawing Circle");
-  }
-}
-class Rectangle extends Shape {
-  @Override 
-  void draw() {
-    System.out.println("Drawing Rectangle");
+class Shape { 
+  // Method for calculating Circle Area
+  double area(double radius) {
+    return Math.PI * radius * radius;
+  } 
+  // Method for calculating Rectangle Area 
+  double area(double length, double width) { 
+    return length * width;
   }
 }
-public class Mouse { 
+public class Main { 
   public static void main(String[] args) { 
-    Shape myShape = new Shape();
-    Shape myCircle = new Circle(); 
-    Shape myRectangle = new Rectangle();
-    
-    myShape.draw();
-    myCircle.draw(); 
-    myRectangle.draw();
+    // Creating Object
+    Shape shape = new Shape();
+    // Calculating Areas
+    double circleArea = shape.area(7.0); 
+    double rectangleArea = shape.area(5.0, 10.0);
+    // Displaying Results
+    System.out.println("Circle Area: " + circleArea);
+    System.out.println("Rectangle Area: " + rectangleArea);
   }
 } 
