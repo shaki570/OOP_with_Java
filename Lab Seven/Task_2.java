5f4ee7949796c9fcfbe671b79a797ac73fee4f6a
Kazi Shaki Jawhar <shakijawhar@gmail.com>
2026-06-22 11:28:14 +0600

    Create Task_2.java

---
+++ Lab Seven/Task_2.java
@@ -0,0 +1,28 @@
+lass Shape { 
+  void draw() { 
+    System.out.println("Drawing Shape"); 
+  }
+} 
+class Circle extends Shape { 
+  @Override 
+  void draw() { 
+    System.out.println("Drawing Circle");
+  }
+}
+class Rectangle extends Shape {
+  @Override 
+  void draw() {
+    System.out.println("Drawing Rectangle");
+  }
+}
+public class Mouse { 
+  public static void main(String[] args) { 
+    Shape myShape = new Shape();
+    Shape myCircle = new Circle(); 
+    Shape myRectangle = new Rectangle();
+    
+    myShape.draw();
+    myCircle.draw(); 
+    myRectangle.draw();
+  }
+} 
